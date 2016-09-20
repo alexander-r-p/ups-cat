3b4cfad2e375f1eaa2ae5731b3601050b2d1c188
alexander-r-p <alexander.r.petrovich@gmail.com>
2016-09-20 13:49:47 +0300

    move config to public

--- config/database.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Alexander on 20.06.2016.
- */
-/*
-module.exports = {
-    url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
-};
-*/
---
+++ public/config/database.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Alexander on 20.06.2016.
+ */
+/*
+module.exports = {
+    url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
+};
+*/
