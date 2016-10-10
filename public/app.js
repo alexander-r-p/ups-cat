c7b4c608c7bb70e2712757bb8ee48c57d67621b3
alexander-r-p <alexander.r.petrovich@gmail.com>
2016-10-10 13:02:09 +0300

    update site

--- public/app.js
+++ public/app.js
@@ -1,1 +1,1 @@
-var app = angular.module('kvgroup-cat', ['store-directives', 'ngResource', 'ngMaterial', 'ngRoute' ]);
+var app = angular.module('kvgroup-cat', ['store-directives', 'ngResource', 'ngMaterial', 'ngRoute', 'ngSanitize' ]);
--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,9 +1,18 @@
 /**
  * Created by Alexander on 20.06.2016.
  */
-app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', function ($scope, translationService, $mdDialog) {
+app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', '$sce', function ($scope, translationService, $mdDialog, $sce) {
     //$scope.imageIndex = 0;
     $scope.currentNavItem = 'home';
+    var currentProduct;
+
+    $scope.readAsHTML = function(snippet) {
+        return readTextAsHTML(snippet);
+    }
+
+    var readTextAsHTML = function(snippet) {
+        return $sce.trustAsHtml(snippet);
+    }
 
     $scope.goto = function(navItem) {
         $scope.currentNavItem = navItem;
@@ -14,11 +23,12 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
         product.currentImageIndex = imageNumber || 0;
     };
 
-    $scope.showAdvanced = function (ev) {
+    $scope.showAdvanced = function (product, ev) {
+        currentProduct = product;
         console.log(ev);
         $mdDialog.show({
                 controller: DialogController,
-                templateUrl: 'dialog1.tmpl.html',
+                templateUrl: 'product_description_dialog.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 clickOutsideToClose:true,
@@ -32,6 +42,14 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
     };
 
     function DialogController($scope, $mdDialog) {
+        $scope.getCurrentProduct = function() {
+            return currentProduct;
+        };
+
+        $scope.parseHTML = function(snippet) {
+            return readTextAsHTML(snippet);
+        };
+
         $scope.hide = function() {
             $mdDialog.hide();
         };
@@ -47,7 +65,7 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
 
     $scope.products = [{
         name: 'UPS Type A 500',
-        description: "Есть стабилизатор, индикатор напряжения.",
+        description: "<ul><li>Стабилизатор</li><li>индикатор напряжения</li></ul>",
         voltage: "12V",
         capacity: "350W",
         maxCapacity: "500W",
@@ -63,7 +81,7 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
         ]
     }, {
         name: 'UPS Type B 700',
-        description: "<ul><li>Есть стабилизатор;</li><li>индикатор напряжения;</li><li>зарядка с функцией «Вампир» для использования различных источников енергии(солнечной, термопреобразователей, доп.аккумулятора для зарядки основного);</li> <li>выход сигнала на включение внешнего генератора електроэнергии;</li><li>выход для подключения GSM сигнализации.</li></ul>",
+        description: "<ul><li>Стабилизатор</li><li>Индикатор напряжения</li><li>Зарядка с функцией «Вампир» для использования различных источников енергии (солнечной, термопреобразователей, доп.аккумулятора для зарядки основного)</li> <li>Выход сигнала на включение внешнего генератора електроэнергии</li><li>Выход для подключения GSM сигнализации</li></ul>",
         voltage: "12V",
         capacity: "450W",
         maxCapacity: "700W",
@@ -79,7 +97,7 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
         ]
     }, {
         name: 'UPS Type C 1000',
-        description: "Есть стабилизатор, индикатор напряжения, солнечная зарядка с функцией PPM (поиска точки максимальной мощности) мощной солнечной батареи, выход сигнала на включение внешнего генератора електроэнергии, выход для подключения GSM сигнализации.",
+        description: "<ul><li>Стабилизатор</li><li>Индикатор напряжения</li><li>Солнечная зарядка с функцией PPM (поиска точки максимальной мощности) мощной солнечной батареи</li><li>Выход сигнала на включение внешнего генератора електроэнергии</li><li>Выход для подключения GSM сигнализации</li></ul>",
         voltage: "12V",
         capacity: "600W",
         maxCapacity: "1000W",
@@ -95,7 +113,7 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
         ]
     }, {
         name: 'UPS Type D 1000',
-        description: "Есть стабилизатор, индикатор напряжения, солнечная зарядка с функцией PPM (поиска точки максимальной мощности) мощной солнечной батареи, выход сигнала на включение внешнего генератора електроэнергии, выход для подключения GSM сигнализации.",
+        description: "<ul><il>Стабилизатор</il><li>Индикатор напряжения</li><li>Солнечная зарядка с функцией PPM (поиска точки максимальной мощности) мощной солнечной батареи</li><li>Выход сигнала на включение внешнего генератора електроэнергии</li><li>Выход для подключения GSM сигнализации</li></ul>",
         voltage: "24V",
         capacity: "700W",
         maxCapacity: "1000W",
