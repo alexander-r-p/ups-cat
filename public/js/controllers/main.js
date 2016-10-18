19caea7feeb94c286f3b81ccad023dd9a83e66d6
Alexander Romanjuk <Alexander.Romanjuk@genesys.com>
2016-10-18 23:59:41 +0300

    add amg on front page

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -5,6 +5,7 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
     //$scope.imageIndex = 0;
     $scope.currentNavItem = 'home';
     var currentProduct;
+    var imgSrc;
 
     $scope.getImageSrc = function(name, index) {
         return name + index + ".jpg";
@@ -27,12 +28,29 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
         product.currentImageIndex = imageNumber || 0;
     };
 
+    $scope.showFullImg = function (index, ev) {
+        imgSrc = $scope.getImageSrc('/img/product_img/img_full_', index+1)
+        $mdDialog.show({
+                controller: DialogController,
+                templateUrl: '/full_img_dialog.html',
+                parent: angular.element(document.body),
+                targetEvent: ev,
+                clickOutsideToClose:true,
+                fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+            })
+            .then(function(answer) {
+                $scope.status = 'You said the information was "' + answer + '".';
+            }, function() {
+                $scope.status = 'You cancelled the dialog.';
+            });
+    };
+
     $scope.showAdvanced = function (product, ev) {
         currentProduct = product;
         console.log(ev);
         $mdDialog.show({
                 controller: DialogController,
-                templateUrl: 'product_description_dialog.html',
+                templateUrl: '/product_description_dialog.html',
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 clickOutsideToClose:true,
@@ -46,6 +64,10 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
     };
 
     function DialogController($scope, $mdDialog) {
+        $scope.getImage = function() {
+            return imgSrc;
+        };
+
         $scope.getCurrentProduct = function() {
             return currentProduct;
         };
@@ -117,7 +139,7 @@ app.controller('StoreController', ['$scope', 'translationService', '$mdDialog', 
         ]
     }, {
         name: 'UPS Type D 1000',
-        description: "<ul><il>Стабилизатор</il><li>Индикатор напряжения</li><li>Солнечная зарядка с функцией PPM (поиска точки максимальной мощности) мощной солнечной батареи</li><li>Выход сигнала на включение внешнего генератора електроэнергии</li><li>Выход для подключения GSM сигнализации</li></ul>",
+        description: "<ul><li>Стабилизатор</li><li>Индикатор напряжения</li><li>Солнечная зарядка с функцией PPM (поиска точки максимальной мощности) мощной солнечной батареи</li><li>Выход сигнала на включение внешнего генератора електроэнергии</li><li>Выход для подключения GSM сигнализации</li></ul>",
         voltage: "24V",
         capacity: "700W",
         maxCapacity: "1000W",
